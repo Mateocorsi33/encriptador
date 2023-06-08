1a483c841a7b6670ba0bb7281cb5fc8ac0ee7343
Mateo <mateofrontend@gmail.com>
2023-06-08 20:41:52 -0300

    nuevo

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const mensaje = document.querySelector(".texto-resultado");
 const inicio = document.querySelector(".texto-inicio")
 const muñeco = document.querySelector(".muñeco")
 
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
 function btnEncriptar(){
     const textoEncriptado = encriptar(textArea.value);
     mensaje.value = textoEncriptado;
@@ -26,7 +20,7 @@ function btnDesencriptar(){
 function btnCopiar(){
     const copiaTexto = document.querySelector(".texto-resultado").value;
     navigator.clipboard.writeText(copiaTexto)
-    textArea.value = "";
+    textArea.value = "hidden";
 }
 
 function encriptar(stringEncriptado){
